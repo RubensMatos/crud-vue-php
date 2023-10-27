fda3ba8a2b65843aadf2e31e629838be125914fb
RubensMatos <rubens.matos.filho@gmail.com>
2023-10-26 21:44:20 -0300

    Dump Database

--- dump-database.sql
+++ dump-database.sql
@@ -1,1 +1,353 @@
-sd
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.0
+-- Dumped by pg_dump version 16.0
+
+-- Started on 2023-10-26 21:43:02
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 222 (class 1259 OID 16427)
+-- Name: order; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."order" (
+    id integer NOT NULL,
+    customer character varying(255),
+    product_data json
+);
+
+
+ALTER TABLE public."order" OWNER TO postgres;
+
+--
+-- TOC entry 221 (class 1259 OID 16426)
+-- Name: order_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.order_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.order_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 4873 (class 0 OID 0)
+-- Dependencies: 221
+-- Name: order_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.order_id_seq OWNED BY public."order".id;
+
+
+--
+-- TOC entry 220 (class 1259 OID 16415)
+-- Name: product; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.product (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL,
+    producttype_id integer,
+    amount numeric(10,2)
+);
+
+
+ALTER TABLE public.product OWNER TO postgres;
+
+--
+-- TOC entry 219 (class 1259 OID 16414)
+-- Name: product_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.product_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.product_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 4874 (class 0 OID 0)
+-- Dependencies: 219
+-- Name: product_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.product_id_seq OWNED BY public.product.id;
+
+
+--
+-- TOC entry 218 (class 1259 OID 16408)
+-- Name: producttype; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.producttype (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL,
+    percentvalue numeric(10,2)
+);
+
+
+ALTER TABLE public.producttype OWNER TO postgres;
+
+--
+-- TOC entry 217 (class 1259 OID 16407)
+-- Name: producttype_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.producttype_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.producttype_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 4875 (class 0 OID 0)
+-- Dependencies: 217
+-- Name: producttype_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.producttype_id_seq OWNED BY public.producttype.id;
+
+
+--
+-- TOC entry 216 (class 1259 OID 16399)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    username character varying(255) NOT NULL,
+    password character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+--
+-- TOC entry 215 (class 1259 OID 16398)
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.users_id_seq OWNER TO postgres;
+
+--
+-- TOC entry 4876 (class 0 OID 0)
+-- Dependencies: 215
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- TOC entry 4707 (class 2604 OID 16430)
+-- Name: order id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."order" ALTER COLUMN id SET DEFAULT nextval('public.order_id_seq'::regclass);
+
+
+--
+-- TOC entry 4706 (class 2604 OID 16418)
+-- Name: product id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.product ALTER COLUMN id SET DEFAULT nextval('public.product_id_seq'::regclass);
+
+
+--
+-- TOC entry 4705 (class 2604 OID 16411)
+-- Name: producttype id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.producttype ALTER COLUMN id SET DEFAULT nextval('public.producttype_id_seq'::regclass);
+
+
+--
+-- TOC entry 4704 (class 2604 OID 16402)
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- TOC entry 4867 (class 0 OID 16427)
+-- Dependencies: 222
+-- Data for Name: order; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."order" (id, customer, product_data) FROM stdin;
+6	João	[{"name":"Produto A","quantity":4,"value":40,"tax":0.2,"valueUnit":10},{"name":"Produto B","quantity":5,"value":100,"tax":1.1,"valueUnit":20}]
+7	João	[{"name":"Produto A","quantity":4,"value":40,"tax":0.2,"valueUnit":10},{"name":"Produto B","quantity":5,"value":100,"tax":1.1,"valueUnit":20}]
+\.
+
+
+--
+-- TOC entry 4865 (class 0 OID 16415)
+-- Dependencies: 220
+-- Data for Name: product; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.product (id, name, producttype_id, amount) FROM stdin;
+18	Pulseira para Relógio	2	15.00
+17	Playstation 5	7	5200.00
+19	Geladeira	7	7000.00
+\.
+
+
+--
+-- TOC entry 4863 (class 0 OID 16408)
+-- Dependencies: 218
+-- Data for Name: producttype; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.producttype (id, name, percentvalue) FROM stdin;
+6	Móveis	1.23
+1	Livros	12.34
+4	Eletrônicos	123.45
+7	Eletrodomésticos	12345.67
+5	Eletroeletrônicos	1.23
+3	Acessórios	10.20
+2	Alimentos	2.00
+\.
+
+
+--
+-- TOC entry 4861 (class 0 OID 16399)
+-- Dependencies: 216
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.users (id, username, password) FROM stdin;
+1	master	123456
+\.
+
+
+--
+-- TOC entry 4877 (class 0 OID 0)
+-- Dependencies: 221
+-- Name: order_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.order_id_seq', 7, true);
+
+
+--
+-- TOC entry 4878 (class 0 OID 0)
+-- Dependencies: 219
+-- Name: product_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.product_id_seq', 19, true);
+
+
+--
+-- TOC entry 4879 (class 0 OID 0)
+-- Dependencies: 217
+-- Name: producttype_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.producttype_id_seq', 13, true);
+
+
+--
+-- TOC entry 4880 (class 0 OID 0)
+-- Dependencies: 215
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 1, true);
+
+
+--
+-- TOC entry 4715 (class 2606 OID 16434)
+-- Name: order order_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."order"
+    ADD CONSTRAINT order_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4713 (class 2606 OID 16420)
+-- Name: product product_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.product
+    ADD CONSTRAINT product_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4711 (class 2606 OID 16413)
+-- Name: producttype producttype_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.producttype
+    ADD CONSTRAINT producttype_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4709 (class 2606 OID 16406)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 4716 (class 2606 OID 16421)
+-- Name: product product_producttype_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.product
+    ADD CONSTRAINT product_producttype_id_fkey FOREIGN KEY (producttype_id) REFERENCES public.producttype(id);
+
+
+-- Completed on 2023-10-26 21:43:02
+
+--
+-- PostgreSQL database dump complete
+--
+
